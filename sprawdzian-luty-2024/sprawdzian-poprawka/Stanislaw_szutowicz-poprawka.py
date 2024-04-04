7803269887b05fa442b1bf6d97c97575cb9d20fd
SPsato <147045879+SPsato@users.noreply.github.com>
2024-04-04 12:24:29 +0200

    Update Stanislaw_szutowicz-poprawka.py

--- sprawdzian-luty-2024/sprawdzian-poprawka/Stanislaw_szutowicz-poprawka.py
+++ sprawdzian-luty-2024/sprawdzian-poprawka/Stanislaw_szutowicz-poprawka.py
@@ -2,10 +2,14 @@ import re
 tekst = "Dokumentacja pojazdów: Ford Mustang VIN: AFAFP404XYF223456 należał do Steve'a, zanim został sprzedany z VIN: W0L0XCE075E033456. W naszej bazie znajdują się również pojazdy importowane takie jak Nissan z VIN: JN1TANR35U0001234."
 a = re.findall(r'VIN[A-Z0-9: ]+', tekst)
 
-check = "A"
+checka = "A"
+checkb = "B"
+checkc = "C"
+checkd = "D"
+checke = "E"
 
 print("The original list : " + str(a))
 
-res = [idx for idx in a if idx[5].lower() == check.lower()]
+res = [idx for idx in a if idx[5].lower() == checka.lower() or idx[5].lower() == checkb.lower() or idx[5].lower() == checkc.lower() or idx[5].lower() == checkd.lower() or idx[5].lower() == checke.lower()]
 
 print("The list of matching first letter : " + str(res))
